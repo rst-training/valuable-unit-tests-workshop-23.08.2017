113d1383d496f9e98e37ca1ee584677d2bf12d06
Krzysztof Menżyk <krzysztof.menzyk@rst.com.pl>
2017-08-23 09:17:04 +0200

    Conference::makeReservationForOrder test cases

--- test/Domain/Reservation/ConferenceTest.php
+++ test/Domain/Reservation/ConferenceTest.php
@@ -6,10 +6,27 @@ use PHPUnit\Framework\TestCase;
 
 class ConferenceTest extends TestCase
 {
-    /**
-     * @todo: remove it
-     */
-    public function test_example_name()
+    public function it_makes_reservation_if_all_seats_are_available()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function it_updates_seats_availability_if_reservation_is_made()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function it_adds_reservation_to_the_wait_list_if_all_seats_have_exact_availability()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function it_adds_reservation_to_the_wait_list_if_at_least_one_seat_is_unavailable()
+    {
+        $this->markTestSkipped();
+    }
+
+    public function make_reservation_fails_if_reservation_already_exists()
     {
         $this->markTestSkipped();
     }
