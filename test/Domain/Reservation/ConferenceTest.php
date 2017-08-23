00ed988600dfa321b10569bfd27dc7c23d99bf8d
kkedzierski <krzysztof.kedzierski@rst.com.pl>
2017-08-23 09:15:56 +0200

    Reservation cancellation test methods added

--- test/Domain/Reservation/ConferenceTest.php
+++ test/Domain/Reservation/ConferenceTest.php
@@ -13,4 +13,52 @@ class ConferenceTest extends TestCase
     {
         $this->markTestSkipped();
     }
+
+    /**
+     * @test
+     */
+    public function should_throw_exception_when_order_does_not_exist()
+    {
+
+    }
+
+    /**
+     * @test
+     */
+    public function should_remove_reservation_when_cancellation_finished()
+    {
+
+    }
+
+    /**
+     * @test
+     */
+    public function should_increment_seats_availability_for_all_types_of_seats_in_reservation()
+    {
+
+    }
+
+    /**
+     * @test
+     */
+    public function should_do_nothing_after_cancellation_when_wait_list_is_empty()
+    {
+
+    }
+
+    /**
+     * @test
+     */
+    public function should_create_reservation_for_first_reservation_from_wait_list_when_its_possible_after_cancellation()
+    {
+
+    }
+
+    /**
+     * @test
+     */
+    public function should_create_reservation_for_all_reservation_from_wait_list_when_its_possible_after_cancellation()
+    {
+
+    }
 }
