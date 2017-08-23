8cb617e5773eb98687aead6ae27fbf06ffff3163
Jacek Mleczek <jacek.mleczek@rst.com.pl>
2017-08-23 09:17:10 +0200

    Add tests for canceling reservation

--- test/Domain/Reservation/ConferenceTest.php
+++ test/Domain/Reservation/ConferenceTest.php
@@ -6,11 +6,28 @@ use PHPUnit\Framework\TestCase;
 
 class ConferenceTest extends TestCase
 {
-    /**
-     * @todo: remove it
-     */
-    public function test_example_name()
+    public function test_should_throw_exception_while_canceling_not_existing_reservation()
     {
-        $this->markTestSkipped();
+
+    }
+
+    public function test_should_release_all_seats_and_remove_reservation_while_canceling_it()
+    {
+
+    }
+
+    public function test_should_move_one_waiting_reservation_while_canceling_other_reservation()
+    {
+
+    }
+
+    public function test_should_move_two_waiting_reservation_while_canceling_other_reservation()
+    {
+
+    }
+
+    public function test_should_not_move_waiting_reservation_while_canceling_other_reservation()
+    {
+
     }
 }
