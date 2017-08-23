3910d384ce8bada3c3ff65857b09b2f1d3010273
Mateusz Piotrowski <mateusz.piotrowski@rst.com.pl>
2017-08-23 09:17:23 +0200

    Add tests description

--- test/Domain/Reservation/ConferenceTest.php
+++ test/Domain/Reservation/ConferenceTest.php
@@ -9,8 +9,51 @@ class ConferenceTest extends TestCase
     /**
      * @todo: remove it
      */
-    public function test_example_name()
+    public function testShouldThrowExceptionForNotFoundOrder()
     {
+        $this->markTestSkipped();
+    }
+
+    public function testShouldIncrementSeatsQuantity() {
+        $this->markTestSkipped();
+    }
+
+    public function testShouldRemoveReservation() {
+        $this->markTestSkipped();
+    }
+
+    // Check if seat is moveable
+    public function testShouldMarkSeatAsNotMoveable() {
+        $this->markTestSkipped();
+    }
+
+    public function testShouldMarkSeatAsMoveable() {
+        $this->markTestSkipped();
+    }
+
+    // Reservation could be moved
+    public function testShouldDecrementSeatQuantity() {
+        $this->markTestSkipped();
+    }
+    
+    public function testShouldAddReservation() {
+        $this->markTestSkipped();
+    }
+
+    public function testShouldRemoveReservationFromWaitList() {
+        $this->markTestSkipped();
+    }
+
+    // Reservation cannot been moved
+    public function testShouldNotDecrementSeatQuantity() {
+        $this->markTestSkipped();
+    }
+
+    public function testShouldNotAddReservation() {
+        $this->markTestSkipped();
+    }
+
+    public function testShouldNotRemoveReservationFromWaitList() {
         $this->markTestSkipped();
     }
 }
